7340b563da75cf6f051d229253ecaa21cca1197b
Y700 <laszlo.vadkert@gmail.com>
2022-03-21 13:05:41 +0100

    detailed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,25 +7,26 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Currency[] currencies = new Currency[]{new EUR(), new HUF()};
         Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-
         System.out.println("Insert your name: ");
         String name = scanner.nextLine();
 
         long startTime = System.nanoTime();
 
+        Currency[] currencies = new Currency[]{new EUR(), new HUF()};
+        Random random = new Random();
         Currency actualCurrency = currencies[random.nextInt(currencies.length)];
 
         int result = 0;
         int actualPrice = actualCurrency.getOriginalPrice();
+        StringBuilder detailResult = new StringBuilder();
 
         for (int i = 0; i < actualCurrency.getBankNotes().length; i++) {
             while (actualPrice >= actualCurrency.getBankNotes()[i]) {
-                int countIntem = actualPrice / actualCurrency.getBankNotes()[i];
+                int countItem = actualPrice / actualCurrency.getBankNotes()[i];
                 actualPrice = actualPrice % actualCurrency.getBankNotes()[i];
-                result += countIntem;
+                result += countItem;
+                detailResult.append(countItem + "x " + actualCurrency.getBankNotes()[i] + ", ");
             }
         }
 
@@ -33,17 +34,17 @@ public class Main {
 
         String fileName = name + ".txt";
         try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileName), "UTF-8"))) {
-            writer.write("Your name: " + name + "\n");
+//            writer.write("Your name: " + name + "\n");
             writer.write("Currency is: " + actualCurrency.getName() + "\n");
             writer.write("Original price: " + actualCurrency.getOriginalPrice() + "\n");
-            writer.write("Result is: " + result + "\n");
+            writer.write("Result: " + result + " --> " + detailResult + "\n");
             writer.write("Time of calculate: " + totalTime + " nanoseconds");
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            e.getMessage();
         } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+            e.getMessage();
         } catch (IOException e) {
-            e.printStackTrace();
+            e.getMessage();
         }
     }
 }
